07db4b7fcb9dced9be3ec2044c070ed21fb7ffcf
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-10 15:07:31 +0900

    記述間違い訂正、保存後トップページが表示されるように記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,17 @@ class PostsController < ApplicationController
     #Postモデル、すなわちpostsテーブルからすべてのデータを取得、インスタンス変数@postsに代入
     #すべてのデータを取得するために、allというActiveRecordのメソッド
   end
-  
+
   def new
   end
 
   def create
+    Post.create(memo: params[:memo])
+    redirect_to "/posts"
+    #モデル名.create(カラム名: 保存する値)
+    #createメソッドの引数には、保存したいカラム名、保存する値をキーとバリューのセットで記述
+    #キーはmemo:,保存したい値に記述しているのはparams[:memo]
+    #「postsテーブルのmemoカラムに、name属性にmemoの値を持っているフォームから送信されたデータを保存する」
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  #  HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  get 'posts', to: 'posts#create'
+  post 'posts', to: 'posts#create'
   # データが投稿されてテーブルに登録する,HTTPメソッドはPOSTを選択.投稿内容を保存してレコードを作成する処理を行うことから、create
 end
