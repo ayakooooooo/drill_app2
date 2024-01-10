0b59f959fa78ab7aea3f0cd882dd292dde529427
ayakooooooo <itoshunitoshunitoshun@gmail.com>
2024-01-10 14:27:57 +0900

    新規投稿内容をデータベースに保存するためのルーティングを

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
     #Postモデル、すなわちpostsテーブルからすべてのデータを取得、インスタンス変数@postsに代入
     #すべてのデータを取得するために、allというActiveRecordのメソッド
   end
+  
   def new
   end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
+  get 'posts', to: 'posts#create'
+  # データが投稿されてテーブルに登録する,HTTPメソッドはPOSTを選択.投稿内容を保存してレコードを作成する処理を行うことから、create
 end
